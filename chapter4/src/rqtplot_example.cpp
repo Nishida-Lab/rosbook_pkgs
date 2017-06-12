b4f068f6d875cf58207320d748a79cdc99c66c90
RyodoTanaka <k104073r@mail.kyutech.jp>
2017-06-13 02:48:48 +0900

    [add] rqtplot_example

---
+++ chapter4/src/rqtplot_example.cpp
@@ -0,0 +1,36 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+
+int main(int argc, char** argv)
+{
+  // ros init
+  ros::init(argc, argv, "rqtplot_example");
+
+  // node handler
+  ros::NodeHandle n;
+  // publisher
+  ros::Publisher pub;
+  pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+
+  // publish message
+  geometry_msgs::Twist cmd_vel;
+
+  // ros rate
+  ros::Rate rate(10);
+
+  // main loop
+  while(ros::ok()){
+	double t = ros::Time::now().toSec();
+	cmd_vel.linear.x = 100.0*sin(t);
+	cmd_vel.linear.y = 100.0*sin(1./2.*t);
+	cmd_vel.linear.z = 100.0*sin(1./5.*t);
+	cmd_vel.angular.x = 100.0*cos(t);
+	cmd_vel.angular.y = 100.0*cos(1./2.*t);
+	cmd_vel.angular.z = 100.0*cos(1./5.*t);
+	pub.publish(cmd_vel);
+	rate.sleep();
+  }
+  
+  return 0;
+}
+
