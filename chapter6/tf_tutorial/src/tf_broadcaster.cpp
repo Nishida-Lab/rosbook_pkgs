e278482749ee5542da4cd3a3dca8fa0a42d71d63
RyodoTanaka <k104073r@mail.kyutech.jp>
2017-06-22 02:12:09 +0900

    [modify] geometry_msgs -> tf class

--- chapter6/tf_tutorial/src/tf_broadcaster.cpp
+++ chapter6/tf_tutorial/src/tf_broadcaster.cpp
@@ -2,7 +2,6 @@
 #include <tf/LinearMath/Quaternion.h>
 #include <tf/LinearMath/Vector3.h>
 #include <tf/transform_broadcaster.h>
-#include <geometry_msgs/TransformStamped.h>
 
 int main(int argc, char** argv)
 {
@@ -11,27 +10,19 @@ int main(int argc, char** argv)
   ros::NodeHandle n;
 
   tf::TransformBroadcaster tb;
-  geometry_msgs::TransformStamped ts;
+  tf::StampedTransform st;
 
   ros::Rate r(1.0);
   
-  while(ros::ok()){
-	ts.header.stamp = ros::Time::now();
-	ts.header.frame_id = "frame1";
-	ts.child_frame_id = "frame2";
+  while(ros::ok()){	
 	tf::Vector3 v;
 	v.setValue(0, 0, 1.0);
-	ts.transform.translation.x = v.x();
-	ts.transform.translation.y = v.y();
-	ts.transform.translation.z = v.z();
+	st.setOrigin(v);
 	tf::Quaternion q;
 	q.setRPY(0, 0, 0);
-	ts.transform.rotation.x = q.x();
-	ts.transform.rotation.y = q.y();
-	ts.transform.rotation.z = q.z();
-	ts.transform.rotation.w = q.w();
-
-	tb.sendTransform(ts);
+	st.setRotation(q);
+	
+	tb.sendTransform(tf::StampedTransform(st, ros::Time::now(), "frame1", "frame2"));
 	ROS_INFO("Transform Published");
 	
 	r.sleep();
