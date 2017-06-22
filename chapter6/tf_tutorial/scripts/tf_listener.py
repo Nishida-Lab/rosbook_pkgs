54a9c091454cf280120825b981d6acc639522119
RyodoTanaka <k104073r@mail.kyutech.jp>
2017-06-22 22:36:56 +0900

    [modify] add conversion from Quaternion to euler

--- chapter6/tf_tutorial/scripts/tf_listener.py
+++ chapter6/tf_tutorial/scripts/tf_listener.py
@@ -17,10 +17,17 @@ if __name__ == '__main__':
             rospy.logerr('LookupTransform Error !')
             rospy.sleep(1.0)
             continue
+        
+        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(rotation, axes='sxyz')
 
         rospy.loginfo("\n=== Got Transform ===\n"
+                      " Translation\n"
                       " x : %f\n y : %f\n z : %f\n"
-                      " x : %f\n y : %f\n z : %f\n w : %f",
+                      " Quaternion\n"
+                      " x : %f\n y : %f\n z : %f\n w : %f\n"
+                      " RPY\n"
+                      " R : %f\n P : %f\n Y : %f",
                       translation[0], translation[1], translation[2],
-                      rotation[0], rotation[1], rotation[2], rotation[3])
+                      rotation[0], rotation[1], rotation[2], rotation[3],
+                      roll, pitch, yaw)
         r.sleep()
