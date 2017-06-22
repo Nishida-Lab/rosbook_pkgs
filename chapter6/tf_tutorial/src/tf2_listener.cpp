f2f2eaf5da988fa2e31be235db320876d9407847
RyodoTanaka <k104073r@mail.kyutech.jp>
2017-06-22 21:59:41 +0900

    [fix] apply tf2 & add conversion

--- chapter6/tf_tutorial/src/tf2_listener.cpp
+++ chapter6/tf_tutorial/src/tf2_listener.cpp
@@ -1,5 +1,8 @@
 #include <ros/ros.h>
-#include <tf/transform_listener.h>
+#include <tf2/LinearMath/Vector3.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
 
 int main(int argc, char** argv)
@@ -18,22 +21,37 @@ int main(int argc, char** argv)
 	try{
 	  transform = tfBuffer.lookupTransform("frame1", "frame2", ros::Time(0));
 	}
-	catch (tf::TransformException &ex){
+	catch (tf2::TransformException &ex){
 	  ROS_ERROR("%s", ex.what());
 	  ros::Duration(1.0).sleep();
 	  continue;
 	}
 
+	tf2::Vector3 translation;
+	translation.setValue(transform.transform.translation.x,
+						 transform.transform.translation.y,
+						 transform.transform.translation.z);
+	
+	tf2::Quaternion rotation;
+	rotation.setValue(transform.transform.rotation.x,
+					  transform.transform.rotation.y,
+					  transform.transform.rotation.z,
+					  transform.transform.rotation.w);
+
+	tf2::Matrix3x3 m(rotation);
+	double roll, pitch, yaw;
+	m.getRPY(roll, pitch, yaw);
+
 	ROS_INFO("\n=== Got Transform ===\n"
+			 " Translation\n"
 			 " x : %f\n y : %f\n z : %f\n"
-			 " x : %f\n y : %f\n z : %f\n w : %f",
-			 transform.transform.translation.x,
-			 transform.transform.translation.y,
-			 transform.transform.translation.z,
-			 transform.transform.rotation.x,
-			 transform.transform.rotation.y,
-			 transform.transform.rotation.z,
-			 transform.transform.rotation.w);
+			 " Quaternion\n"
+			 " x : %f\n y : %f\n z : %f\n w : %f\n"
+			 " RPY\n"
+			 " R : %f\n P : %f\n Y : %f",
+			 translation.x(), translation.y(), translation.z(),
+			 rotation.x(), rotation.y(), rotation.z(), rotation.w(),
+			 roll, pitch, yaw);
 	
 	r.sleep();
   }
