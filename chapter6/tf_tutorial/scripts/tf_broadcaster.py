c87a4b34268cc5e90e3385baa6ff9f832cd623d8
RyodoTanaka <k104073r@mail.kyutech.jp>
2017-06-22 11:03:50 +0900

    [add] tf broadcaster python example

---
+++ chapter6/tf_tutorial/scripts/tf_broadcaster.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import rospy
+import tf
+
+if __name__ == '__main__':
+    rospy.init_node('tf_broadcaster')
+
+    br = tf.TransformBroadcaster()
+
+    r = rospy.Rate(1.0)
+    
+    while not rospy.is_shutdown():
+        origin = (0, 0, 1.0)
+        rotation = tf.transformations.quaternion_from_euler(0,0,0, axes='sxyz')
+        br.sendTransform(origin, rotation,
+                         rospy.Time.now(),'frame2', 'frame1')
+        rospy.loginfo('Transform Published')
+        r.sleep()
