ed7d4d84c78c1b1e2bf044d0fe7d3825cdec1986
MoriKen254 <masamorobo@gmail.com>
2017-06-26 09:47:52 +0900

    Add launch and rviz for each sample

--- chapter14/cloud_excercise/src/cloud_clusterer.cpp
+++ chapter14/cloud_excercise/src/cloud_clusterer.cpp
@@ -4,6 +4,7 @@
 
 #include <pcl/kdtree/kdtree.h>
 #include <pcl/segmentation/extract_clusters.h>
+#include <pcl/filters/extract_indices.h>
 
 #include <cloud_exercise/operation_handler.h>
 
@@ -60,19 +61,22 @@ private:
         ec.extract(cluster_indices);
     }
 
-    void extractCluster(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_ptr, std::vector<pcl::PointIndices> cluster_indices)
+    void extractCluster(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered_pcl_ptr, std::vector<pcl::PointIndices> &cluster_indices)
     {
-        for(std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+        for(std::vector<pcl::PointIndices>::iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
         {
-          pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZ>);
-          for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
-            cloud_cluster->points.push_back (cloud_filtered_ptr->points[*pit]);
-          cloud_cluster->width = cloud_cluster->points.size ();
-          cloud_cluster->height = 1;
-          cloud_cluster->is_dense = true;
+          pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster_pcl_ptr (new pcl::PointCloud<pcl::PointXYZ>);
+          pcl::ExtractIndices<pcl::PointXYZ> extract;
+          pcl::PointIndices::Ptr inliers(new pcl::PointIndices(*it));
+          // Create the filtering object
+          extract.setInputCloud(cloud_filtered_pcl_ptr->makeShared());
+          extract.setIndices(inliers);
+          // Extract the cluster inlier pointcloud from indices
+          extract.setNegative(false);
+          extract.filter(*cloud_cluster_pcl_ptr);
 
           sensor_msgs::PointCloud2 cloud_cluster_ros;
-          pcl::toROSMsg(*cloud_cluster, cloud_cluster_ros);
+          pcl::toROSMsg(*cloud_cluster_pcl_ptr, cloud_cluster_ros);
           cloud_cluster_ros.header.frame_id = "base_link";
 
           int index = it - cluster_indices.begin();
--- chapter14/cloud_excercise/src/cloud_planar_segmenter.cpp
+++ chapter14/cloud_excercise/src/cloud_planar_segmenter.cpp
@@ -12,7 +12,8 @@ class CloudPlanarSegmenter : public CloudOperator
 public:
     CloudPlanarSegmenter(ros::NodeHandle &nh) :
         cloud_segmented_pub_(nh.advertise<sensor_msgs::PointCloud2>("cloud_segmented", 1)),
-        cloud_without_segmented_pub_(nh.advertise<sensor_msgs::PointCloud2>("cloud_without_segmented", 1))
+        cloud_without_segmented_pub_(nh.advertise<sensor_msgs::PointCloud2>("cloud_without_segmented", 1)),
+        coefficients_pub_(nh.advertise<pcl_msgs::ModelCoefficients>("coefficients", 1))
     {}
 
     void operate()
@@ -26,6 +27,7 @@ public:
     {
         cloud_segmented_pub_.publish(cloud_segmented_ros_);
         cloud_without_segmented_pub_.publish(cloud_without_segmented_ros_);
+        coefficients_pub_.publish(coefficients_ros_);
     }
 
 private:
@@ -33,7 +35,7 @@ private:
     {
         pcl::fromROSMsg(cloud_input_ros_, cloud_input_pcl_);
 
-        pcl::ModelCoefficients coefficients;
+        pcl::ModelCoefficients coefficients_pcl;
         pcl::SACSegmentation<pcl::PointXYZ> segmentation;
 
         // Create the segmentation object
@@ -42,7 +44,9 @@ private:
         segmentation.setMaxIterations(1000);
         segmentation.setDistanceThreshold(0.01);
         segmentation.setInputCloud(cloud_input_pcl_.makeShared());
-        segmentation.segment(*inliers, coefficients);
+        segmentation.segment(*inliers, coefficients_pcl);
+
+        pcl_conversions::fromPCL(coefficients_pcl, coefficients_ros_);
     }
 
     void extract(pcl::PointIndices::Ptr inliers)
@@ -54,9 +58,10 @@ private:
         // Create the filtering object
         extract.setInputCloud(cloud_input_pcl_.makeShared());
         extract.setIndices(inliers);
+        // Extract the plannar inlier pointcloud from indices
         extract.setNegative(false);
         extract.filter(cloud_segmented_pcl);
-
+        // Remove the plannar inlier pointcloud, extract the rest
         extract.setNegative(true);
         extract.filter(cloud_without_segmented_pcl);
 
@@ -68,8 +73,10 @@ private:
 protected:
     ros::Publisher cloud_segmented_pub_;
     ros::Publisher cloud_without_segmented_pub_;
+    ros::Publisher coefficients_pub_;
     sensor_msgs::PointCloud2 cloud_segmented_ros_;
     sensor_msgs::PointCloud2 cloud_without_segmented_ros_;
+    pcl_msgs::ModelCoefficients coefficients_ros_;
     pcl::PointCloud<pcl::PointXYZ> cloud_input_pcl_;
 };
 
