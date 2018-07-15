943713847b3e7c172fadcb39c72dd2759bc91309
yamacir-kit <httperror@404-notfound.jp>
2018-07-15 13:34:49 +0900

    Fix cording style
    インデントの修正，タブをスペースに変更．

--- chapter6/tf_tutorial/src/tf_broadcaster.cpp
+++ chapter6/tf_tutorial/src/tf_broadcaster.cpp
@@ -3,6 +3,7 @@
 #include <tf/LinearMath/Vector3.h>
 #include <tf/transform_broadcaster.h>
 
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "tf_broadcaster");
@@ -13,20 +14,22 @@ int main(int argc, char** argv)
   tf::StampedTransform st;
 
   ros::Rate r(1.0);
-  
-  while(ros::ok()){	
-	tf::Vector3 translation;
-	translation.setValue(0, 0, 1.0);
-	st.setOrigin(translation);
-	
-	tf::Quaternion rotation;
-	rotation.setRPY(0, 0, 0);
-	st.setRotation(rotation);
-	
-	tb.sendTransform(tf::StampedTransform(st, ros::Time::now(), "frame1", "frame2"));
-	ROS_INFO("Transform Published");
-	
-	r.sleep();
+
+  while(ros::ok())
+  {
+    tf::Vector3 translation;
+    translation.setValue(0, 0, 1.0);
+    st.setOrigin(translation);
+
+    tf::Quaternion rotation;
+    rotation.setRPY(0, 0, 0);
+    st.setRotation(rotation);
+
+    tb.sendTransform(tf::StampedTransform(st, ros::Time::now(), "frame1", "frame2"));
+    ROS_INFO("Transform Published");
+
+    r.sleep();
   }
+
   return 0;
 }
