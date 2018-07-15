f126f1a0a0eaf5e90542973df7ec1ab30bac7684
yamacir-kit <httperror@404-notfound.jp>
2018-07-15 13:17:30 +0900

    Fix cording style
    ヘッダファイルの並び順変更，タブをスペースに置き換え，インデントの修正．
    
    また，イコールの位置を揃えて，演算子の両脇をきちんと開けるようにした．
    浮動小数点の小数点以下の省略は初心者に混乱をもたらすだけだと考え，省略しない形に修正

--- chapter4/src/rqtplot_example.cpp
+++ chapter4/src/rqtplot_example.cpp
@@ -1,5 +1,7 @@
 #include <ros/ros.h>
+
 #include <geometry_msgs/Twist.h>
+
 
 int main(int argc, char** argv)
 {
@@ -8,6 +10,7 @@ int main(int argc, char** argv)
 
   // node handler
   ros::NodeHandle n;
+
   // publisher
   ros::Publisher pub;
   pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
@@ -19,18 +22,19 @@ int main(int argc, char** argv)
   ros::Rate rate(10);
 
   // main loop
-  while(ros::ok()){
-	double t = ros::Time::now().toSec();
-	cmd_vel.linear.x = 100.0*sin(t);
-	cmd_vel.linear.y = 100.0*sin(1./2.*t);
-	cmd_vel.linear.z = 100.0*sin(1./5.*t);
-	cmd_vel.angular.x = 100.0*cos(t);
-	cmd_vel.angular.y = 100.0*cos(1./2.*t);
-	cmd_vel.angular.z = 100.0*cos(1./5.*t);
-	pub.publish(cmd_vel);
-	rate.sleep();
+  while (ros::ok())
+  {
+    double t = ros::Time::now().toSec();
+    cmd_vel.linear.x  = 100.0 * sin(t);
+    cmd_vel.linear.y  = 100.0 * sin(1.0 / 2.0 * t);
+    cmd_vel.linear.z  = 100.0 * sin(1.0 / 5.0 * t);
+    cmd_vel.angular.x = 100.0 * cos(t);
+    cmd_vel.angular.y = 100.0 * cos(1.0 / 2.0 * t);
+    cmd_vel.angular.z = 100.0 * cos(1.0 / 5.0 * t);
+    pub.publish(cmd_vel);
+    rate.sleep();
   }
-  
+
   return 0;
 }
 
