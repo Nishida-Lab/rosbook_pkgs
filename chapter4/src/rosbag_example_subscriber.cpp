34affe176f3a3e4f5e73311645061cbf608f4212
yamacir-kit <httperror@404-notfound.jp>
2018-07-15 13:15:43 +0900

    Fix cording style
    ヘッダファイルの並び順の変更とインデントおよび空行の調整，

--- chapter4/src/rosbag_example_subscriber.cpp
+++ chapter4/src/rosbag_example_subscriber.cpp
@@ -1,17 +1,20 @@
-#include <ros/ros.h>
-#include <std_msgs/Char.h>
-#include <std_msgs/Float64.h>
+#include <iostream>
 #include <random>
 
-#include <iostream>
+#include <ros/ros.h>
+
+#include <std_msgs/Char.h>
+#include <std_msgs/Float64.h>
+
 
 class RosbagExample
 {
 public:
   RosbagExample();
+
 private:
   void Callback(const std_msgs::Char::ConstPtr& msg);
-  
+
   ros::Publisher pub;
   ros::Subscriber sub;
   ros::NodeHandle n;
@@ -19,6 +22,7 @@ private:
   double average;
   double data_num;
 };
+
 
 RosbagExample::RosbagExample() :
   average(0),
@@ -28,20 +32,19 @@ RosbagExample::RosbagExample() :
   sub = n.subscribe<std_msgs::Char>("/random_num", 1, &RosbagExample::Callback, this);
 }
 
-
 void RosbagExample::Callback(const std_msgs::Char::ConstPtr& msg)
 {
   // Publish message
   std_msgs::Float64 pub_msg;
 
   // calculate the average
-  average += (double(msg->data) - average) / (data_num + 1.);
+  average += (double(msg->data) - average) / (data_num + 1.0);
   pub_msg.data = average;
   data_num++;
-  
+
   // Publish
   pub.publish(pub_msg);
-  ROS_INFO("Published"); 
+  ROS_INFO("Published");
 }
 
 
@@ -49,11 +52,13 @@ int main(int argc, char** argv)
 {
   // ros init
   ros::init(argc, argv, "rosbag_example_subscriber");
+
   // class init
   RosbagExample rosbag_example;
+
   // start to subscribe
   ros::spin();
-  
+
   return 0;
 }
 
